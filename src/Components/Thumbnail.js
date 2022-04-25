f04a061bbd9d213caa08341e21480fe479e1f1b7
Tara Culpin <taraculpin@gmail.com>
2022-04-25 20:26:15 +0100

    project grid responsive and titles contained within the card

--- src/Components/Thumbnail.js
+++ src/Components/Thumbnail.js
@@ -4,13 +4,20 @@ import '../App.css';
 
 function Thumbnail(props) {
   return (
-    <div className="project">
+    <div className="project"
+    style={{
+      backgroundImage: `url(${props.image})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center"
+    }}>
       <Link to={props.link}>
-        <div className="project-image">
-          <img src={props.image} alt="Project" />
+        {/* <div className="project-image"> */}
+          {/* <img src={props.image} alt="Project" /> */}
+        {/* </div> */}
+        <div class="project-title">
+          <h2>{props.title}</h2>
+          <h3>{props.category}</h3>
         </div>
-        <div className="project-title">{props.title}</div>
-        <div className="project-category">{props.category}</div>
       </Link>
     </div>
   );
