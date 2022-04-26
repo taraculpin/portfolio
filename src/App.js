f2fe2a909d55f87c8fed1765b66cc46cb93321fc
Tara Culpin <taraculpin@gmail.com>
2022-04-26 08:27:18 +0100

    added home page

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Projects from './Components/Projects';
 import Articles from './Components/Articles';
 import About from './Components/About';
+import Home from './Components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <nav className="navigation">
+        <Link to="/" className="item">
           <img src={logo} className="logo-image" alt="Logo" />
+          </Link>
           <ul>
-            <li><Link to="/" className="item">Projects</Link></li>
+            <li><Link to="projects" className="item">Projects</Link></li>
             <li><Link to="articles" className="item">Articles</Link></li>
             <li><Link to="about" className="item">About</Link></li>
           </ul>
       </nav>
       <Routes>
-        <Route path="/" element={<Projects />} />
+        <Route path="/" element={<Home />} />
+        <Route path="projects/*" element={<Projects />} />
         <Route path="articles" element={<Articles />} />
         <Route path="about" element={<About />} />
       </Routes>
